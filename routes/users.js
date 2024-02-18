f9df2e278973ccedd7bd4e41603afd254965a389
KRISHNA PRASANTH S <52340052+krish280700@users.noreply.github.com>
2024-02-17 20:13:10 -0600

    ITMD542 | Contacts UI

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+let data = []
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/contacts', function(req, res, next) {
+  res.render('contacts', { title: 'Contacts', contacts: data.length ? data : 'No data found'});
 });
 
 module.exports = router;
